988d59c57cf2aa6b124ef08f6b74e0b59475b23d
vvsy <ayau911@gmail.com>
2025-05-28 17:03:50 +0800

    add a keymap for removing all trailing whitespace in current file

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -3,3 +3,7 @@ require("options")
 require("plugins.lazy")
 require("plugins.keymaps")
 require("plugins.options")
+
+-- remove all trailing whitespace
+vim.keymap.set('n', '<leader>tW', ':%s/\\s\\+$//e<CR>', { silent = true })
+
